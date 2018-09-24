2aa63460f2918385e7299d955079986b7c1c9888
songjunjie <songjunjie@advanpro.hk>
2018-09-24 16:20:39 +0800

    deploy

---
+++ precache-manifest.1b1c301a7c6f9956e746eeff0981b87a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chat-kungfu/robots.txt"
+  },
+  {
+    "revision": "7253e3206b7e993947ba",
+    "url": "/chat-kungfu/js/chunk-vendors.55fcb56e.js"
+  },
+  {
+    "revision": "c7955175c7b9e1905091",
+    "url": "/chat-kungfu/js/app.29d80bcf.js"
+  },
+  {
+    "revision": "02acb1a6bee8c74b7db63d0bf8db4db5",
+    "url": "/chat-kungfu/index.html"
+  },
+  {
+    "revision": "49767ac507f1808d28b1122464ebb4e1",
+    "url": "/chat-kungfu/img/texture.49767ac5.png"
+  },
+  {
+    "revision": "c7955175c7b9e1905091",
+    "url": "/chat-kungfu/css/app.224ee201.css"
+  }
+];
